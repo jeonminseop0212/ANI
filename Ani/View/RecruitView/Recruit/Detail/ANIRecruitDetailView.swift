bd1091adfee1625f8689944effd23c2b2aa977df
jeonminseop0212 <jeonminseop0212@gmail.com>
2018-10-12 11:29:21 +0900

    recruit detail gradation

--- Ani/View/RecruitView/Recruit/Detail/ANIRecruitDetailView.swift
+++ Ani/View/RecruitView/Recruit/Detail/ANIRecruitDetailView.swift
@@ -101,6 +101,13 @@ class ANIRecruitDetailView: UIView {
     headerImageView.height(headerImageViewHeight)
     self.headerImageView = headerImageView
     
+    //gradiationLayer
+    let gradiationLayer = CAGradientLayer()
+    let margin: CGFloat = 20.0
+    gradiationLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIViewController.STATUS_BAR_HEIGHT + UIViewController.NAVIGATION_BAR_HEIGHT + margin)
+    gradiationLayer.colors = [ANIColor.dark.withAlphaComponent(0.15).cgColor, ANIColor.dark.withAlphaComponent(0).cgColor]
+    headerImageView.layer.addSublayer(gradiationLayer)
+    
     //scrollView
     let scrollView = UIScrollView()
     scrollView.delegate = self
